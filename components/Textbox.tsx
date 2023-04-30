c3b31667decabaf242336bdaff79a042ca78df68
Aggestor <aggeer.aa@gmail.com>
2023-04-30 20:52:49 +0300

    add: useForm hook

--- components/Textbox.tsx
+++ components/Textbox.tsx
@@ -8,10 +8,13 @@ declare type Props = {
     label:string
     name:string
 }
-export const Textbox : FunctionComponent<Props>  = ({label}) =>{
+export const Textbox : FunctionComponent<Props>  = ({label, type = "text", value,onChange,name}) =>{
 
-    return <div className="h-8 py-1 px-2 border focus-within:ring-2 group transition-all duration-500 text-gray-600 focus-within:ring-indigo-600 focus-within:text-indigo-600 rounded-md relative">
-        <label htmlFor="" className="absolute  text-sm -top-[12px]  bg-white left-3 group-focus:font-semibold px-1">{label}</label>
-        <input type="text" className="bg-transparent w-full h-[90%] focus:outline-0 mt-1" />
+    return <div className="transition-all duration-500 text-gray-600  focus-within:text-indigo-600">
+        <label htmlFor={name} className="  bg-white left-3 group-focus:font-semibold px-1">{label}</label>
+        <div className="h-9 py-1 px-2 border focus-within:ring-2 focus-within:ring-indigo-600 group  rounded-md">
+            <input onChange={onChange} type={type} value={value} className="bg-transparent w-full h-full focus:outline-0" name={name} id={name} />
+        </div>
     </div>
+    
 }
